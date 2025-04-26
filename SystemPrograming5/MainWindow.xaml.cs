0ef426a0d8266dcc137a95b9e34153241066fa23
Oleksandr-Zabo <zoleksandr06@gmail.com>
2025-04-26 13:48:57 +0300

    ex. 2 had done

--- SystemPrograming5/MainWindow.xaml.cs
+++ SystemPrograming5/MainWindow.xaml.cs
@@ -1,23 +1,81 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
-namespace SystemPrograming5;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace SystemPrograming5
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseSource_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                SourcePath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void BrowseDestination_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                DestinationPath.Text = saveFileDialog.FileName;
+            }
+        }
+
+        private void StartCopy_Click(object sender, RoutedEventArgs e)
+        {
+            string sourceFile = SourcePath.Text;
+            string destinationFile = DestinationPath.Text;
+            if (!File.Exists(sourceFile) || string.IsNullOrEmpty(destinationFile) || !int.TryParse(ThreadCount.Text, out int threadCount))
+            {
+                MessageBox.Show("Invalid inputs!");
+                return;
+            }
+
+            long fileSize = new FileInfo(sourceFile).Length;
+            ProgressBar.Maximum = fileSize;
+
+            Task.Run(() => CopyFile(sourceFile, destinationFile, threadCount));
+        }
+
+        private void CopyFile(string sourceFile, string destinationFile, int threadCount)
+        {
+            long fileSize = new FileInfo(sourceFile).Length;
+            long chunkSize = fileSize / threadCount;
+
+            using (FileStream sourceStream = new FileStream(sourceFile, FileMode.Open, FileAccess.Read))
+            using (FileStream destStream = new FileStream(destinationFile, FileMode.Create, FileAccess.Write))
+            {
+                object progressLock = new object();
+                Parallel.For(0, threadCount, i =>
+                {
+                    long start = chunkSize * i;
+                    long end = (i == threadCount - 1) ? fileSize : chunkSize * (i + 1);
+
+                    sourceStream.Seek(start, SeekOrigin.Begin);
+                    byte[] buffer = new byte[end - start];
+                    sourceStream.Read(buffer, 0, buffer.Length);
+
+                    lock (progressLock)
+                    {
+                        destStream.Seek(start, SeekOrigin.Begin);
+                        destStream.Write(buffer, 0, buffer.Length);
+                        Dispatcher.Invoke(() => ProgressBar.Value += buffer.Length);
+                    }
+                });
+            }
+
+            Dispatcher.Invoke(() => MessageBox.Show("Copy completed!"));
+        }
     }
 }
